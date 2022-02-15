74b3da0cc6f59ea1295a9f0afdc10abeb1cac5aa
thepicture <85333076+thepicture@users.noreply.github.com>
2022-02-15 10:52:37 +0000

    Implement the services controller

---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,44 @@
+﻿using MedicalLaboratoryNumber20WebAPI.Models.Entities;
+using MedicalLaboratoryNumber20WebAPI.Models.ResponseModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace MedicalLaboratoryNumber20WebAPI.Controllers
+{
+    public class ServicesController : ApiController
+    {
+        private readonly MedicalLaboratoryNumber20Entities db =
+            new MedicalLaboratoryNumber20Entities();
+
+        // GET: api/Services
+        public IEnumerable<ResponseService> GetService()
+        {
+            return db.Service.ToList()
+                .ConvertAll(s => new ResponseService(s));
+        }
+
+        // GET: api/Services/5
+        [ResponseType(typeof(Service))]
+        public async Task<IHttpActionResult> GetService(int id)
+        {
+            Service service = await db.Service.FindAsync(id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(new ResponseService(service));
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/ResponseModels/ResponseService.cs
@@ -0,0 +1,18 @@
+﻿using MedicalLaboratoryNumber20WebAPI.Models.Entities;
+
+namespace MedicalLaboratoryNumber20WebAPI.Models.ResponseModels
+{
+    public class ResponseService
+    {
+        public int Code;
+        public string Title;
+        public decimal PriceInRubles;
+
+        public ResponseService(Service service)
+        {
+            Code = service.Code;
+            Title = service.ServiceName;
+            PriceInRubles = service.PriceInRubles;
+        }
+    }
+}
